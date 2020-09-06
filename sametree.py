8efa2c12de7217616f9d813109de593266b2d59b
Zaid Khan <zaidcomps@gmail.com>
2020-09-06 17:18:02 +0530

    sametree

---
+++ sametree.py
@@ -0,0 +1,29 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isTreeSame(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
+        if (p is None and q is None):
+            return True
+        
+        if (p is None and q is not None) or (p is not None and q is None):
+            return False
+
+        if p.val != q.val:
+            return False
+        
+        if self.isTreeSame(p.left, q.left):
+            return self.isTreeSame(p.right, q.right)
+        
+        return False
+
